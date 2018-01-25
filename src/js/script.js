6e3b2f3a1d273d06e381cfcc4239bf8e54aec24d
Mahreen Qureshi <mahreenq@hotmail.com>
2018-01-24 19:04:08 -0500

    cleaned commented code

--- src/js/script.js
+++ src/js/script.js
@@ -11,66 +11,43 @@ $.ajax({
 		return { country: key, rate: result.rates[key]};
 	});
 
-		console.log(output);
+
+			var fromDiv = document.getElementById("from");
+
+			var fromselectList = document.createElement("select");
+			fromselectList.id = "fromSelect";
+			fromDiv.appendChild(fromselectList);
 
 
-
-var fromDiv = document.getElementById("from");
-
-var fromselectList = document.createElement("select");
-fromselectList.id = "fromSelect";
-fromDiv.appendChild(fromselectList);
-
-
-for (var i = 0; i < output.length; i++) {
-    var option = document.createElement("option");
-    option.value = output[i].rate;
-    option.text = output[i].country;
-    fromselectList.appendChild(option);
-}
-
+			for (var i = 0; i < output.length; i++) {
+				var option = document.createElement("option");
+				option.value = output[i].rate;
+				option.text = output[i].country;
+				fromselectList.appendChild(option);
+			}
 
 
 
 
-var toDiv = document.getElementById("to");
+			var toDiv = document.getElementById("to");
 
-var toselectList = document.createElement("select");
-toselectList.id = "toSelect";
-toDiv.appendChild(toselectList);
+			var toselectList = document.createElement("select");
+			toselectList.id = "toSelect";
+			toDiv.appendChild(toselectList);
 
-for (var i = 0; i < output.length; i++) {
-    var option = document.createElement("option");
-    option.value = output[i].rate;
-    option.text = output[i].country;
-    toselectList.appendChild(option);
-}
+			for (var i = 0; i < output.length; i++) {
+				var option = document.createElement("option");
+				option.value = output[i].rate;
+				option.text = output[i].country;
+				toselectList.appendChild(option);
+			}
 
-var euro = document.createElement("euro");
-euro.value = 1;
-euro.text = "EUR";
-toselectList.appendChild(euro);
-
-// var fromCurrency=document.getElementById("fromSelect").value ;
-// console.log(fromCurrency);
-
-// var toCurrency=document.getElementById("toSelect").value ;
-// console.log(toCurrency);
-
-// var toConvert =document.getElementById("toConvert").value ;
-// console.log(toConvert);
+			var euro = document.createElement("euro");
+			euro.value = 1;
+			euro.text = "EUR";
+			toselectList.appendChild(euro);
 
 
-
-
- 
-// document.getElementById("demo").onclick = doFunction;
-
-// function doFunction(){
-// 	 var newValue = toConvert * toCurrency
-// 	 console.log(newValue);
-
-// }
 
 
   }).fail(function(err) {
